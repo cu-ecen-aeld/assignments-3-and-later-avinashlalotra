1bb55ee9544cce68591fee6ef6dc07c0861f59bc
avinashlalotra <abinashsinghlalotra@gmail.com>
2024-09-08 23:47:56 +0530

    Test success

---
+++ finder-app/finder.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+filesdir="$1"
+searchstr="$2"
+
+# Check if directory and search string are provided
+if [ -z "$filesdir" ]; then
+    echo "filesdir is not specified"
+    exit 1
+fi
+
+if [ -z "$searchstr" ]; then
+    echo "search string is not specified"
+    exit 1
+fi
+
+# Check if the directory is valid
+if [ ! -d "$filesdir" ]; then
+    echo "Error: '$filesdir' is not a valid directory"
+    exit 1
+fi
+
+# Count the number of files
+files_number=$(find "$filesdir" -type f | wc -l)
+
+# Count the number of matching lines
+matching_lines=$(grep -r -l "$searchstr" "$filesdir" | wc -l )
+
+# Print results
+echo "The number of files are ${files_number} and the number of matching lines are ${matching_lines:-0}"
+
+exit 0
+
---
+++ finder-app/writer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+	echo "Usage: $0 <filepath> <text_string>"
+	exit 1
+fi
+
+writefile="$1"
+writestr="$2"
+
+dirpath=$(dirname "$writefile")
+
+if [ ! -d "$dirpath" ]; then
+	if ! mkdir -p "$dirpath" ; then
+		echo "Error : Could not create directory $dirpath "
+		exit 1
+	fi
+fi
+
+if ! echo "$writestr" > "$writefile" ; then
+	echo "Error: Could not write to file $writefile "
+	exit 1
+fi
+
+exit 0
